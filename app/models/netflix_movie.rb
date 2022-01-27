f604e041e9c0809472bc08afc0f0a01039a50f9c
Igor Areco <igorhareco@gmail.com>
2022-01-27 16:37:10 -0400

    modelo criado

---
+++ app/models/netflix_movie.rb
@@ -0,0 +1,2 @@
+class NetflixMovie < ApplicationRecord
+end
---
+++ db/migrate/20220127203041_create_netflix_movies.rb
@@ -0,0 +1,14 @@
+class CreateNetflixMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :netflix_movies do |t|
+      t.string :title
+      t.string :genre
+      t.integer :year
+      t.string :country
+      t.string :published_at
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
